5fd336b1b9e7536527e012c995e30caeb2e5cfd4
DevinThePancake <DevinThePancake@users.noreply.github.com>
2018-02-21 08:35:25 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # azlyricsman
-Gets the lyrics from https://azlyrics.com
+[![npm package](https://nodei.co/npm/azlyricsman.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/azlyricsman/)
+
+[![Build Status](https://travis-ci.org/DevinThePancake/azlyricsman.svg?branch=master)](https://travis-ci.org/DevinThePancake/azlyricsman)
+[![Dependency Status](https://img.shields.io/david/devinthepancake/azlyricsman.svg?style=flat-square)](https://david-dm.org/devinthepancake/azlyricsman)
+
+This uses [azlyrics](https://azlyrics.com/) and gets the song of your choice and returns a promise with song lyrics, title, and song
+
+Usage:
+```js
+const fetchSong = require('azlyricsman')
+
+fetchSong('Meme machine').then(song => console.log(song.lyrics))
+//Fetches the song you give and returns the lyrics 
+
+fetchSong('Meme machine').then(song => console.log(song.title))
+//Fetches the song you give and returns the title
+
+fetchSong('Meme machine').then(song => console.log(song.artist))
+//Fetches the song you give and returns the artist
+
+fetchSong('Meme machine').then(song => console.log(`Lyrics for the song ${song.title} by ${song.artist}\n\n${song.lyrics}`))
+//This is noice
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const request = require('request')
+const cheerio = require('cheerio')
+
+module.exports = {
+    lyrics: async(query) => {
+    return new Promise((resolve, reject) => {
+    if(!query) reject('You must input a song name to search')
+const res = await request('https://search.azlyrics.com/search.php?q='+query.replace(/\s/g, '+'))
+    const $ = cheerio.load(res)
+    let lyricLink = $("div.col-xs-12.col-sm-offset-1.col-md-8.col-md-offset-2.text-center").find('div.panel').eq(2).find("td.text-left.visitedlyr").find("a").eq(0).attr('href')
+  if(lyricLink === undefined) lyricLink = $("div.col-xs-12.col-sm-offset-1.col-md-8.col-md-offset-2.text-center").find('div.panel').eq(1).find("td.text-left.visitedlyr").find("a").eq(0).attr('href')
+  if(lyricLink === undefined) lyricLink = $("div.col-xs-12.col-sm-offset-1.col-md-8.col-md-offset-2.text-center").find('div.panel').eq(0).find("td.text-left.visitedlyr").find("a").eq(0).attr('href')
+  if(lyricLink === undefined) reject("No songs found")
+const re = await request(lyricLink)
+    const o = cheerio.load(re)
+  let artist = o("div.col-xs-12.col-lg-8.text-center").find("div").eq(4).text().replace('Lyrics', '').replace("\n", '').replace("\n", '')
+  let song = o("div.col-xs-12.col-lg-8.text-center").find("div").eq(3).text().replace('lyrics', '').replace('"', '').replace('"', '')
+  let lyrics = o("div.col-xs-12.col-lg-8.text-center").find("div").eq(6).text()
+  let info = {
+      "lyrics": lyrics,
+      "artist": artist,
+      "title": song
+  }
+ resolve(info)
+})
+}
+}
